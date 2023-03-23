edc46c7e0aadea988355edf7cf48633ecca7c435
Clinton Brito <clintoncsb@gmail.com>
2023-03-22 22:01:45 -0300

    Resolvendo os 2 primeiros requisitos v2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -3,9 +3,13 @@ export const fetchProduct = () => {
 };
 
 export async function fetchProductsList(query) {
-  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-  if (!query) throw new Error('Termo de busca não informado');
-  return fetch(url)
-    .then((res) => res.json())
-    .then((data) => data.results);
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+    if (!query) throw new Error('Termo de busca não informado');
+    return fetch(url)
+      .then((res) => res.json())
+      .then((data) => data.results);
+  } catch (err) {
+    console.error(err);
+  }
 }
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,7 +5,7 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    expect(fetchProductsList).toBe('function');
+    expect(typeof fetchProductsList).toBe('function');
   });
 
   it('fetch é chamado ao executar fetchProductsList', () => {
@@ -18,7 +18,7 @@ describe('Teste a função fetchProductsList', () => {
   });
 
   it('ao chamar a função fetchProductsList sem argumento, retorna um erro com a mensagem: `Termo de busca não informado`', async () => {
-    const emptyFunction = await fetchProductsList()
-    expect ((emptyFunction).toThrowError('Termo de busca não informado'));
+    // const emptyFunction = await fetchProductsList();
+    await expect(fetchProductsList()).toThrowError('Termo de busca não informado');
   });
 });
